88b415d6cb616ad8a3ccf9b2795fe187ad650355
afrianjunior <afrian.junior26@gmail.com>
2018-09-12 21:32:30 +0700

    MODIFIED: port

--- webpack.development.js
+++ webpack.development.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
-    port: process.env.PORT || 9000,
+    port: process.env.PORT || 4000,
     compress: true
   }
 })
