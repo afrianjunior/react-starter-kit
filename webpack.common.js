a8857079302886babcbd6fbbe1796cc7b92bfbaa
afrianjunior <afrian.junior26@gmail.com>
2018-09-12 21:32:18 +0700

    ADD: code spliting vendors

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
-    app: './src/index.jsx'
+    app: './src/index.jsx',
+    vendors: ['react', 'react-dom', 'react-redux', 'react-router-dom', 'redux', 'redux-thunk', 'styled-components']
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
