4ca86006302acad2a4ce307c1a3af8100c1ba8f4
afrianjunior <afrian.junior26@gmail.com>
2018-09-12 21:32:00 +0700

    MODIFIED: port config

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 app.use('*', (res, req) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
@@ -12,4 +12,4 @@ app.use('*', (res, req) => {
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => console.log(`[App] running http:localhost:${PORT}`));
+app.listen(PORT, () => console.log('\x1b[32m%s\x1b[0m', `[App] running http:localhost:${PORT}`))
