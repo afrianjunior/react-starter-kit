90769fa2fccb9c5cb879261586ad9477b8bd0401
afrianjunior <afrian.junior26@gmail.com>
2018-09-12 21:16:14 +0700

    MODIFIED: add mode development

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import App from './App'
+import App from './app/App'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk'
 import rootReducer from './reducer'
 
+const devTool = window => {
+  if (process.env.NODE_ENV === 'development') return window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+}
+
 const store = createStore(
   rootReducer,
   applyMiddleware(reduxThunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  devTool(window)
 )
 
 export default function Root () {
