03d893b7f9f3feb4467466e2656bb708febd9a92
afrianjunior <afrian.junior26@gmail.com>
2018-09-15 11:42:50 +0700

    ADD: handle all undefined route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,7 @@ import { Layout } from 'ui'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
+import NotFound from './pages/utils/NotFound'
 export default function App () {
   return (
     <Router>
@@ -11,6 +12,7 @@ export default function App () {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/detail" component={Detail} />
+          <Route exact path="*" component={NotFound} />
         </Switch>
       </Layout>
     </Router>
